be776118edf9e7b75f6e0e7a61effcf82e50e92b
liufeihe <lfhe2014@163.com>
2019-08-01 16:02:01 +0800

    add filterableProductTable

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Comment from './Comment';
 import Clock from './Clock';
 import Toggle from './Toggle';
 import Calculator from './Calculator';
+import FilterableProductTable from './FilterableProductTable';
 
 class App extends Component {
   render(){
@@ -16,6 +17,14 @@ class App extends Component {
       name: 'peter',
       avatarUrl: '../public/favicon.ico'
     }
+    const PRODUCTS = [
+      {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+      {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+      {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+      {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+      {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+      {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+    ];
     return (
       <div className="App">
         <div className="Table">
@@ -27,6 +36,7 @@ class App extends Component {
         <Clock />
         <Toggle />
         <Calculator />
+        <FilterableProductTable products={PRODUCTS} />
       </div>
     )
   }
